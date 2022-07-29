1d684be525febe5abee5ba7780cd5d49b4d8e67e
Joel Brinson <jtbrinson11@gmail.com>
2022-07-29 11:24:29 -0400

    added files

---
+++ src/main/java/com/GenSpark/RESTFulApp_5/RestFulApp5Application.java
@@ -0,0 +1,13 @@
+package com.GenSpark.RESTFulApp_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulApp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/GenSpark/RESTFulApp_5/RestFulApp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GenSpark.RESTFulApp_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulApp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
